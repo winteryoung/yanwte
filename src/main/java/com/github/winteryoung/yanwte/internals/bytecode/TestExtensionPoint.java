b255dde9d938bb2f45d4c8dc07e50a13f5c57dca
Winter Young <513805252@qq.com>
2016-01-23 02:05:39 +0800

    remove unused code

--- src/main/java/com/github/winteryoung/yanwte/internals/bytecode/TestExtensionPoint.java
+++
@@ -1,9 +0,0 @@
-package com.github.winteryoung.yanwte.internals.bytecode;
-
-/**
- * @author Winter Young
- * @since 2016/1/22
- */
-public interface TestExtensionPoint {
-    void foo(Integer arg1, Long arg2);
-}
