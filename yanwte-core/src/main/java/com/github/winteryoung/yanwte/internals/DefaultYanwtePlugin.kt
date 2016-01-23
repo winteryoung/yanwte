90bd37fb2c4844ff78808d5d6e844b72abe1875a
Winter Young <513805252@qq.com>
2016-01-23 21:45:16 +0800

    make yanwte a multi-module project

--- yanwte-core/src/main/java/com/github/winteryoung/yanwte/internals/DefaultYanwtePlugin.kt
+++ yanwte-core/src/main/java/com/github/winteryoung/yanwte/internals/DefaultYanwtePlugin.kt
@@ -6,7 +6,7 @@ import com.github.winteryoung.yanwte.YanwtePlugin
  * This Yanwte plugin implementation does single classloader loading, and
  * the objects like extension points and extensions are created by this
  * plugin. It's suitable for library jars, which no multiple classloaders are
- * needed, and objects creations are not managed by external contains like Spring.
+ * needed, and objects creations are not managed by external containers like Spring.
  *
  * @author Winter Young
  * @since 2016/1/21
@@ -17,17 +17,17 @@ internal class DefaultYanwtePlugin : YanwtePlugin {
     }
 
     private fun createInstance(name: String): Any {
-        Thread.currentThread().contextClassLoader?.let { cccl ->
+        Thread.currentThread().contextClassLoader?.let { ccl ->
             val c: Class<*>
             try {
-                c = cccl.loadClass(name)
+                c = ccl.loadClass(name)
                 return c.newInstance()
             } catch (e: ClassNotFoundException) {
                 // try next
             }
         }
 
-        val cl = DefaultYanwtePlugin::class.java.classLoader
+        val cl = this.javaClass.classLoader
         val c = cl.loadClass(name)
         return c.newInstance()
     }
